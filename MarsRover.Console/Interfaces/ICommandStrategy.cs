45ebe2baaac0c3f53ab2618400205b1fb149e039
Levent Tugay Kaplan <leventtugaykaplan@gmail.com>
2022-02-16 11:19:05 +0300

    Create interfaces one for strategy pattern and another location rover

---
+++ MarsRover.Console/Interfaces/ICommandStrategy.cs
@@ -0,0 +1,9 @@
+﻿using MarsRover.Entities;
+
+namespace MarsRover.Interfaces
+{
+    public interface ICommandStrategy
+    {
+        void RunCommand(Rover rover);
+    }
+}
---
+++ MarsRover.Console/Interfaces/ILocator.cs
@@ -0,0 +1,9 @@
+﻿using MarsRover.Entities;
+
+namespace MarsRover.Interfaces
+{
+    public interface ILocator
+    {
+        void LocateRover(Rover rover, Plateau plateau, string commands);
+    }
+}
